e0509e378c272122e413374890e27000d598db9f
brightcoding <elearndevelopement@gmail.com>
2019-09-30 03:14:17 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { TableComponent } from './table/table.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent,
+    TableComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
